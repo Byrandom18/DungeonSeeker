407d64459e18a1a4448968b34656d31a6cfdcf1a
Byrandom18 <byrandom18@mail.ru>
2026-02-01 00:14:44 +0500

    Sword trail

--- Assets/Scripts/Player/Weapons/SwordVisual.cs
+++ Assets/Scripts/Player/Weapons/SwordVisual.cs
@@ -4,7 +4,7 @@ public class SwordVisual : MonoBehaviour
 {
     private Animator animator;
     [SerializeField] private Sword sword;
-
+    [SerializeField] private TrailRenderer trailRenderer;
     private const string ATTACK = "Attack"; //animator trigger
 
     private void Awake()
@@ -14,11 +14,32 @@ public class SwordVisual : MonoBehaviour
 
     private void Start()
     {
+        CheckAllComponents();
+
         sword.OnSwordSwing += Sword_OnSwordSwing;
+    }
+
+    private void CheckAllComponents()
+    {
+        CheckComponent(animator, "Animator");
+        CheckComponent(trailRenderer, "TrailRenderer");
+        CheckComponent(sword, "Sword");
+    }
+
+    private void CheckComponent<T>(T component, string componentName) where T : Component
+    {
+        if (component == null)
+            Debug.LogError($"{componentName} is missing on {gameObject.name}");
     }
 
     private void Sword_OnSwordSwing(object sender, System.EventArgs e)
     {
         animator.SetTrigger(ATTACK);
+        trailRenderer.emitting = true;
+    }
+
+    public void StopEmmiting()
+    {
+        trailRenderer.emitting = false;
     }
 }
